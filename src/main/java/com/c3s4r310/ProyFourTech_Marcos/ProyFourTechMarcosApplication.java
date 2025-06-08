bb7226a3310491c7f74488543df7d394ffad0cb9
César Urbina <cu046481@gmail.com>
2025-06-08 10:07:58 -0500

    Subo proyecto Maven con archivo pom.xml listo

---
+++ src/main/java/com/c3s4r310/ProyFourTech_Marcos/ProyFourTechMarcosApplication.java
@@ -0,0 +1,13 @@
+package com.c3s4r310.ProyFourTech_Marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyFourTechMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyFourTechMarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c3s4r310/ProyFourTech_Marcos/ProyFourTechMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3s4r310.ProyFourTech_Marcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyFourTechMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
